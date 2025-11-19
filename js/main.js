789fb9c70b20bb09540443622aa60a515351e033
Rizky subekty <rizky.subekty@iconpln.co.id>
2025-11-20 00:14:29 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  // Fungsi untuk memuat komponen HTML ke dalam elemen yang ditentukan
+  async function loadComponent(selector, filePath) {
+    const el = document.querySelector(selector);
+    if (!el) return;
+    try {
+      const res = await fetch(filePath);
+      if (!res.ok) throw new Error(`Gagal memuat ${filePath}`);
+      const html = await res.text();
+      el.innerHTML = html;
+    } catch (err) {
+      console.error("Error memuat komponen:", filePath, err);
+    }
+  }
+
+  // Tentukan basePath berdasarkan URL
+  const basePath = (() => {
+    const pathname = window.location.pathname;
+
+    if (pathname.includes("/en/")) {
+      return "../../../";
+    }
+
+    if (pathname.includes("/about-us")) {
+      return "..";
+    }
+
+    return ".";
+  })();
+
+  // Muat header dan footer
+  // await loadComponent("header", `${basePath}/components/header.html`);
+  // await loadComponent("footer", `${basePath}/components/footer.html`);
+
+
+
+});
+
+
